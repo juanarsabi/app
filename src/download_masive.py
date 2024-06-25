ba5f21a311c6f3663bfd6c095799c129dfdf193e
Juan Quesada <165961016+juanarsabi@users.noreply.github.com>
2024-06-25 10:00:06 -0500

    [UPDATE] logica de descarga de videos

--- src/download_masive.py
+++ src/download_masive.py
@@ -1,16 +1,19 @@
 import re
 import os
+import time
+import requests
+import pandas as pd
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 load_dotenv()
 
+URL=os.getenv('URL')
 USUARIO=os.getenv('USUARIO')
 CONTRASENA=os.getenv('CONTRASENA')
-URL=os.getenv('URL')
 URL_PROJECTOS=os.getenv('URL_PROJECTOS')
+CARPETA_VIDEOS='/videos/'
 
 driver = webdriver.Chrome()
 
@@ -23,6 +26,22 @@ def buscar_coincidencias(texto, lista):
         if not elemento.get_attribute("class").endswith(texto):
             coincidencias.append(elemento)
     return coincidencias
+
+def descargar_video(url, carpeta_salida): #TODO: Utilizar metodo de descarga de videos
+    try:
+        respuesta = requests.get(url)
+        nombre_archivo = os.path.basename(url)
+
+        if not os.path.exists(carpeta_salida):
+            os.makedirs(carpeta_salida)
+
+        ruta_salida = os.path.join(carpeta_salida, nombre_archivo)
+        with open(ruta_salida, 'wb') as archivo_salida:
+            archivo_salida.write(respuesta.content)
+
+        print(f'Archivo descargado en {ruta_salida}')
+    except Exception as e:
+        print(f'Error al descargar el archivo: {e}')
 
 def iniciar_sesion():
     driver.get(URL)
@@ -61,12 +80,42 @@ def entrar_proyectos():
             print('Formato no valido')
         time.sleep(8.2)
 
+def iterar_columnas(total_videos):
+    videos_descargar = []
+    videos_descargados = []
+    columnas = driver.find_elements(By.XPATH, '/html/body/form/div[4]/div/div/div/section/article/div/div[1]/div[4]/div[3]/div')
 
-
+    #while: total_videos > len(videos_descargados):
+    for columna in columnas:
+        columna_id = columna.find_element(By.XPATH, './div[2]')
+        print(f'- {columna_id.text}')
+        if (columna_id.text.endswith("Participant ID")):
+            print('Columna ignorada')
+        if (columna_id.text.endswith("eye")):
+            print('Video listo para la descarga')
+            videos_descargar.append(columna)
+    print(f'\nVideos a descargar: {len(videos_descargar)}\n')
+    time.sleep(1)
+    if(len(videos_descargar) > 0):
+        for video in videos_descargar:
+            video_parte = video.find_element(By.XPATH, './div[4]/div')
+            video_link = video.find_element(By.XPATH, './div[7]')
+            video_link.click()
+            time.sleep(1.5)
+            try:
+                popup = driver.find_element(By.CLASS_NAME, 'messagebox-container')
+                popup_close = popup.find_element(By.XPATH, './div/div[1]/div/span/i')
+                popup_video = popup.find_element(By.XPATH, './div/div[2]/div/div/video/source')
+                print(f'Video: {popup_video.get_attribute("src")}')
+                popup_close.click()
+            except Exception as e:
+                print(f'No encontre el popup o hubo un error: {e}')
+            print(f'Video: {video_link.text}')
 
 iniciar_sesion()
 obtener_proyectos()
 entrar_proyectos()
+iterar_columnas(22)
 driver.quit()
 
 # def descargar_video(url, nombre_archivo):
